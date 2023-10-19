29d7be5584e72fc51648e141be2a7d4f8ee14862
Sohoad <102650432+Sohoad@users.noreply.github.com>
2023-10-19 14:30:23 +0800

    Update 10.19f6ebd3.js

--- assets/js/10.19f6ebd3.js
+++ assets/js/10.19f6ebd3.js
@@ -1,1 +1,74 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{291:function(t,e,r){"use strict";r.r(e);var s=r(17),a=Object(s.a)({},(function(){var t=this,e=t._self._c;return e("ContentSlotsDistributor",{attrs:{"slot-key":t.$parent.slotKey}},[e("h1",{attrs:{id:"初雨团队"}},[e("a",{staticClass:"header-anchor",attrs:{href:"#初雨团队"}},[t._v("#")]),t._v(" 初雨团队")]),t._v(" "),e("p",[t._v("新官网正在施工中，请稍等……")]),t._v(" "),e("h2",{attrs:{id:"dism"}},[e("a",{staticClass:"header-anchor",attrs:{href:"#dism"}},[t._v("#")]),t._v(" DISM++")]),t._v(" "),e("ul",[e("li",[t._v("最新稳定版本："),e("a",{attrs:{href:"https://github.com/Chuyu-Team/Dism-Multi-language/releases/latest",target:"_blank",rel:"noopener noreferrer"}},[t._v("https://github.com/Chuyu-Team/Dism-Multi-language/releases/latest"),e("OutboundLink")],1)])]),t._v(" "),e("h2",{attrs:{id:"中文用户群"}},[e("a",{staticClass:"header-anchor",attrs:{href:"#中文用户群"}},[t._v("#")]),t._v(" 中文用户群")]),t._v(" "),e("ul",[e("li",[t._v("QQ: 361689191")]),t._v(" "),e("li",[t._v("Telegram: "),e("a",{attrs:{href:"https://t.me/dismpluschinese",target:"_blank",rel:"noopener noreferrer"}},[t._v("https://t.me/dismpluschinese"),e("OutboundLink")],1)])]),t._v(" "),e("h2",{attrs:{id:"开源社区群"}},[e("a",{staticClass:"header-anchor",attrs:{href:"#开源社区群"}},[t._v("#")]),t._v(" 开源社区群")]),t._v(" "),e("ul",[e("li",[t._v("QQ: 633710173")]),t._v(" "),e("li",[t._v("Telegram: "),e("a",{attrs:{href:"https://t.me/chuyuoss",target:"_blank",rel:"noopener noreferrer"}},[t._v("https://t.me/chuyuoss"),e("OutboundLink")],1)])])])}),[],!1,null,null,null);e.default=a.exports}}]);
+(window.webpackJsonp = window.webpackJsonp || [])
+.push([
+	[10], {
+		291: function(t, e, r) {
+			"use strict";
+			r.r(e);
+			var s = r(17),
+				a = Object(s.a)({}, (function() {
+					var t = this,
+						e = t._self._c;
+					return e("ContentSlotsDistributor", {
+						attrs: {
+							"slot-key": t.$parent.slotKey
+						}
+					}, [e("h1", {
+						attrs: {
+							id: "Sohoads Team"
+						}
+					}, [e("a", {
+						staticClass: "header-anchor",
+						attrs: {
+							href: "#Sohoads Team"
+						}
+					}, [t._v("#")]), t._v(" Sohoads Team")]), t._v(" "), e("p", [t._v("新官网正在施工中，请稍等……")]), t._v(" "), e("h2", {
+						attrs: {
+							id: "dism"
+						}
+					}, [e("a", {
+						staticClass: "header-anchor",
+						attrs: {
+							href: "#dism"
+						}
+					}, [t._v("#")]), t._v(" DESTINYMOD++")]), t._v(" "), e("ul", [e("li", [t._v("最新稳定版本："), e("a", {
+						attrs: {
+							href: "https://github.com/Xposed-Modules-Repo/xdc.destinyImm/releases/latest",
+							target: "_blank",
+							rel: "noopener noreferrer"
+						}
+					}, [t._v("https://github.com/Xposed-Modules-Repo/xdc.destinyImm/releases/latest"), e("OutboundLink")], 1)])]), t._v(" "), e("h2", {
+						attrs: {
+							id: "中文用户群"
+						}
+					}, [e("a", {
+						staticClass: "header-anchor",
+						attrs: {
+							href: "#中文用户群"
+						}
+					}, [t._v("#")]), t._v(" 中文用户群")]), t._v(" "), e("ul", [e("li", [t._v(" ")]), t._v(" "), e("li", [t._v("Telegram: "), e("a", {
+						attrs: {
+							href: "https://t.me/DestinyModChatCn",
+							target: "_blank",
+							rel: "noopener noreferrer"
+						}
+					}, [t._v("https://t.me/dismpluschinese"), e("OutboundLink")], 1)])]), t._v(" "), e("h2", {
+						attrs: {
+							id: "基安技术交流"
+						}
+					}, [e("a", {
+						staticClass: "header-anchor",
+						attrs: {
+							href: "#基安技术交流"
+						}
+					}, [t._v("#")]), t._v(" 基安技术交流")]), t._v(" "), e("ul", [e("li", [t._v(" ")]), t._v(" "), e("li", [t._v("Telegram: "), e("a", {
+						attrs: {
+							href: "https://pd.qq.com/s/gacaf5jwq",
+							target: "_blank",
+							rel: "noopener noreferrer"
+						}
+					}, [t._v("https://t.me/chuyuoss"), e("OutboundLink")], 1)])])])
+				}), [], !1, null, null, null);
+			e.default = a.exports
+		}
+	}
+]);
